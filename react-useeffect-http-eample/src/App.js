9e03511936a6b133eb849df29e200b171fb9d6b5
Ankit Sharma <62986952+ankitsharm877@users.noreply.github.com>
2020-08-24 19:05:54 +0530

    Add files via upload

---
+++ react-useeffect-http-eample/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataFetching from './components/DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
